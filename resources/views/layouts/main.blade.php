d91995df35f974226390ec8c92d095394f902638
Mingchao Liao <mingchaoliao95@gmail.com>
2017-07-06 01:14:09 -0400

    update assets.css version to avoid old cache

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -17,7 +17,7 @@
     <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i"
           rel="stylesheet">
 
-    <link rel="stylesheet" href="/css/assets.css">
+    <link rel="stylesheet" href="/css/assets.css?v=1.0.0">
 
     @yield('css')
 </head>
