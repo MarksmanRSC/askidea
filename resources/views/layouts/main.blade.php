9a5f02e2a7dd86bbb9c63ba2da267950b54187de
Mingchao Liao <mingchaoliao95@gmail.com>
2017-07-06 01:24:53 -0400

    hot fix: change jquery cdn link to https

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -121,7 +121,7 @@
     </div>
 </footer>
 
-<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 <script src="/js/jquery.ajax-cross-origin.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
